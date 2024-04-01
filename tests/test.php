5b090a7ac06e30a8e9f795dd8844b398afc2b832
Chau Ngoc Long <longcn@vietec.com.vn>
2024-04-01 14:58:34 +0700

    add testing

---
+++ tests/test.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use Chaungoclong\PathToRegex\RouteRegexFactory;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$routeRegex = RouteRegexFactory::create(
+    '/posts/{postId}/comments/{commentId?}'
+);
+
+// case full route parameters
+$matchesFullParameter = $routeRegex->match('/posts/1/comments/10');
+var_dump($matchesFullParameter);
+
+// case has only required parameters with splash at the end
+$matchesOnlyRequiredParameterWithSplashAtEnd = $routeRegex->match(
+    '/posts/1/comments/'
+);
+
+var_dump($matchesOnlyRequiredParameterWithSplashAtEnd);
+
+// case has only required parameters
+$matchesOnlyRequiredParameter = $routeRegex->match('/posts/1/comments/');
+
+var_dump($matchesOnlyRequiredParameter);
